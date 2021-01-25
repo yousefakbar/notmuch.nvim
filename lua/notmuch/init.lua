b30e9627dfd0cfe1daa671482b7ac370ce1476a7
Yousef Akbar <yousef@yhakbar.com>
2021-01-25 03:38:57 -0800

    Fix: Procedure for opening and populating buffers
    Use vim api functions to make new notmuch buffers and populate them with
    text.
    
     1. Eliminates the need to use non-blocking key input functions.
     2. More reliably captures output text from shell commands.

--- lua/notmuch/init.lua
+++ lua/notmuch/init.lua
@@ -4,16 +4,13 @@ local f = require'notmuch.float'
 local m = require'notmuch.msg'
 local u = require'notmuch.util'
 
-local buf_stack = {}
-
 nm.show_all_tags = function()
   local buf = v.nvim_create_buf(true, true)
   v.nvim_buf_set_name(buf, "Tags")
   v.nvim_win_set_buf(0, buf)
-  local tags = u.capture("notmuch search --output=tags '*'")
-  v.nvim_buf_set_lines(0, 0, 0, true, u.split(tags))
-  v.nvim_input("gg")
-  v.nvim_del_current_line()
+  v.nvim_command("silent 0read! notmuch search --output=tags '*'")
+  v.nvim_win_set_cursor(0, { 1, 0})
+  v.nvim_buf_set_lines(buf, -2, -1, true, {})
   vim.bo.filetype = "notmuch-hello"
   vim.bo.modifiable = false
   print("Welcome to Notmuch.nvim! Choose a tag to search it.")
@@ -24,12 +21,11 @@ nm.show_tag = function(tag)
   local buf = v.nvim_create_buf(true, true)
   v.nvim_buf_set_name(buf, "tag:" .. tag)
   v.nvim_win_set_buf(0, buf)
-  local out = u.capture("notmuch search tag:" .. tag)
-  v.nvim_buf_set_lines(0, 0, 0, true, u.split(out))
+  v.nvim_command("silent 0read! notmuch search tag:" .. tag)
+  v.nvim_win_set_cursor(0, { 1, 0})
+  v.nvim_buf_set_lines(buf, -2, -1, true, {})
   vim.bo.filetype = "notmuch-threads"
-  v.nvim_del_current_line()
   vim.bo.modifiable = false
-  v.nvim_input("gg")
 end
 
 nm.search_terms = function(search)
@@ -37,12 +33,11 @@ nm.search_terms = function(search)
   local buf = v.nvim_create_buf(true, true)
   v.nvim_buf_set_name(buf, search)
   v.nvim_win_set_buf(0, buf)
-  local out = u.split(u.capture("notmuch search " .. search))
-  v.nvim_buf_set_lines(0, 0, 0, true, out)
+  v.nvim_command("silent 0read! notmuch search " .. search)
+  v.nvim_win_set_cursor(0, { 1, 0})
+  v.nvim_buf_set_lines(buf, -2, -1, true, {})
   vim.bo.filetype = "notmuch-threads"
-  v.nvim_del_current_line()
   vim.bo.modifiable = false
-  v.nvim_input("gg")
 end
 
 -- TODO: implement folding for each message in a thread
@@ -50,14 +45,14 @@ end
 nm.show_thread = function()
   local line = v.nvim_get_current_line()
   local threadid = string.match(line, "[0-9a-z]+", 7)
-  local thread = u.capture("notmuch show thread:" .. threadid .. "| sed 's///g'")
   local buf = v.nvim_create_buf(true, true)
   v.nvim_buf_set_name(buf, "thread:" .. threadid)
   v.nvim_win_set_buf(0, buf)
   v.nvim_command("0read! notmuch show thread:" .. threadid .. " | sed 's///g'")
   v.nvim_command('silent %s/message}//')
   v.nvim_command('silent g/^[a-z]\\+[{}]/d')
-  v.nvim_input("gg")
+  v.nvim_win_set_cursor(0, { 1, 0})
+  v.nvim_buf_set_lines(buf, -2, -1, true, {})
   vim.bo.filetype="mail"
   vim.bo.modifiable = false
 end
